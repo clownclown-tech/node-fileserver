5780cdf7803b1447d601b96954e62c007f4298a8
philipp kern <philippk8@gmail.com>
2023-07-13 13:56:02 +0200

    working fileserver

--- server.js
+++ server.js
@@ -6,25 +6,37 @@ const app = express();
 const upload = multer({ dest: 'uploads/' });
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+
+let uploadedFiles = [];
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.post('/upload', upload.single('file'), (req, res) => {
   if (req.file) {
-    res.send('Datei erfolgreich hochgeladen');
+    uploadedFiles.push(req.file);
+    res.redirect('/');
   } else {
     res.status(400).send('Es wurde keine Datei ausgewählt');
   }
 });
 
-app.get('/download', (req, res) => {
-  const filePath = path.join(__dirname, 'uploads', 'example.txt');
-  res.download(filePath, 'example.txt');
+app.get('/download/:filename', (req, res) => {
+  const { filename } = req.params;
+  const filePath = path.join(__dirname, 'uploads', filename);
+  res.download(filePath);
 });
 
-
-
-
-
+app.get('/uploaded', (req, res) => {
+  res.send(uploadedFiles);
+});
 
 app.listen(3000, () => {
   console.log('Server gestartet auf Port 3000');
 });
+
+console.log(uploadedFiles);
