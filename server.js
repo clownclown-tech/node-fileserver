fc8ea510b8852693b54df7daa45c6f8914cdba05
philipp kern <philippk8@gmail.com>
2023-07-13 20:09:46 +0200

    array for uploaded files persistent

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const upload = multer({ dest: 'uploads/' });
@@ -9,7 +10,44 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 
+
+
+const filePath = 'uploadedFiles.json';
 let uploadedFiles = [];
+
+// Load the uploaded files from the file on server start
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err);
+
+      saveUploadedFiles(); // Create a new file if it doesn't exist
+
+  } else {
+    try {
+      uploadedFiles = JSON.parse(data);
+      console.log('Uploaded files loaded successfully!');
+    } catch (error) {
+      console.error('Error parsing JSON:', error);
+    }
+  }
+ });
+
+// Save the uploaded files to the file
+function saveUploadedFiles() {
+  const jsonString = JSON.stringify(uploadedFiles);
+  fs.writeFile(filePath, jsonString, 'utf8', (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+    } else {
+      console.log('Uploaded files saved successfully!');
+    }
+  });
+}
+
+
+
+
+
 
 
 app.get('/', (req, res) => {
@@ -19,6 +57,7 @@ app.get('/', (req, res) => {
 app.post('/upload', upload.single('file'), (req, res) => {
   if (req.file) {
     uploadedFiles.push(req.file);
+    saveUploadedFiles();
     res.redirect('/');
   } else {
     res.status(400).send('Es wurde keine Datei ausgewählt');
@@ -38,5 +77,3 @@ app.get('/uploaded', (req, res) => {
 app.listen(3000, () => {
   console.log('Server gestartet auf Port 3000');
 });
-
-console.log(uploadedFiles);
